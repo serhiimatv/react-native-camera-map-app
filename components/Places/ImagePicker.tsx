f0076040fa8ce984d05c8911e59850fee39b47ba
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-28 01:04:16 +0200

    integrate expo image picker

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,13 +1,79 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, Image } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
+
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (cameraPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant camera permissions to use this app.',
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  const pickImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+    if (image.assets?.[0]?.uri) {
+      setPickedImage(image.assets[0].uri);
+    }
+  };
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
+  }
   return (
     <View>
-      <Text>ImagePicker</Text>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <Button title="Take Image" onPress={pickImageHandler} />
     </View>
   );
 };
 
 export default ImagePicker;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 4,
+  },
+});
--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
+
 import { Colors } from '../../constants/colors';
+import ImagePicker from './ImagePicker';
 
 const PlaceForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -19,6 +21,7 @@ const PlaceForm = () => {
           style={styles.input}
         />
       </View>
+      <ImagePicker />
     </ScrollView>
   );
 };
