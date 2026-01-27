4c2c161b6ee1a8f05bd852b03f3763ea82f05a79
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-27 23:46:34 +0200

    create image picker file

---
+++ components/Places/ImagePicker.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImagePicker = () => {
+  return (
+    <View>
+      <Text>ImagePicker</Text>
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({});
