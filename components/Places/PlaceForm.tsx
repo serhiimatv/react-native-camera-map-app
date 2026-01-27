7d472b68498966afa36b74088a06aecb2a663b34
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-27 23:06:24 +0200

    Implement PlaceForm component with title input and styling enhancements

--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
+import { Colors } from '../../constants/colors';
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  const changeTitleHandler = (text: string) => {
+    setEnteredTitle(text);
+  };
+
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          value={enteredTitle}
+          onChangeText={changeTitleHandler}
+          style={styles.input}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderRadius: 6,
+    backgroundColor: Colors.primary100,
+  },
+});
