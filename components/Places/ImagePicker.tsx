284d9a16578c6241200a5438c168cb571be8d528
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-02-02 23:50:46 +0200

    Add image picker and permission handling

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,13 +1,102 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Platform,
+  Alert,
+  Image,
+} from 'react-native';
+import { launchCamera } from 'react-native-image-picker';
+import { PERMISSIONS, RESULTS, check, request } from 'react-native-permissions';
+import { Colors } from '../../constants/colors';
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
+
+  const checkCameraPermission = async () => {
+    if (Platform.OS === 'ios') {
+      const iosResult = await check(PERMISSIONS.IOS.CAMERA);
+      if (iosResult === RESULTS.GRANTED) {
+        return true;
+      } else {
+        const iosResultRequest = await request(PERMISSIONS.IOS.CAMERA);
+        if (iosResultRequest === RESULTS.GRANTED) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      const androidResult = await check(PERMISSIONS.ANDROID.CAMERA);
+      if (androidResult === RESULTS.GRANTED) {
+        return true;
+      } else {
+        const androidResultRequest = await request(PERMISSIONS.ANDROID.CAMERA);
+        if (androidResultRequest === RESULTS.GRANTED) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  };
+
+  const verifyPermissions = async () => {
+    const hasPermission = await checkCameraPermission();
+    if (!hasPermission) {
+      Alert.alert(
+        'Permission denied',
+        'You need to grant permission to use the camera',
+      );
+      return false;
+    }
+    return true;
+  };
+  const pickImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCamera({
+      mediaType: 'photo',
+      quality: 0.5,
+      maxWidth: 400,
+      maxHeight: 400,
+    });
+    if (image.assets?.[0]?.uri) {
+      setPickedImage(image.assets[0].uri);
+    }
+  };
+
+  let imagePreview = <Text>No image picked yet.</Text>;
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
   return (
     <View>
-      <Text>ImagePicker</Text>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <Button title="Pick Image" onPress={pickImageHandler} />
     </View>
   );
 };
 
 export default ImagePicker;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 300,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 4,
+  },
+});
--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
 import { Colors } from '../../constants/colors';
+import ImagePicker from './ImagePicker';
 
 const PlaceForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -19,6 +20,7 @@ const PlaceForm = () => {
           style={styles.input}
         />
       </View>
+      <ImagePicker />
     </ScrollView>
   );
 };
