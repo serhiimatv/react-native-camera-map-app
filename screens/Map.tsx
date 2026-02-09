3df37462201c53bae97074d35f37e8fbfefb16ac
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-02-09 23:46:48 +0200

    use env variables for maps api key, add marker on map

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,19 +1,46 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, {
+  MapPressEvent,
+  Marker,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+
   const initialRegion = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
+  const selectLocationHandler = (event: MapPressEvent) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat, lng });
+  };
   return (
     <MapView
       style={styles.map}
       initialRegion={initialRegion}
       provider={PROVIDER_GOOGLE}
-    />
+      mapPadding={{ top: 0, right: 0, bottom: 25, left: 0 }}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
   );
 };
 
